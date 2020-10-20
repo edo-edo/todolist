70b61404a92c36257e96178e6a70a905677a3f5a
edo-edo <giorgiediberidze99@gmail.com>
2020-10-20 22:01:49 +0400

    start server side rendering

--- server/server.js
+++ server/server.js
@@ -1,8 +1,14 @@
+import React from 'react';
+import App from '../client/App';
+
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const passport = require('passport');
 const signale = require('signale');
+const path = require('path');
+const fs = require('fs');
+const ReactDOMServer = require('react-dom/server');
 
 require('dotenv').config();
 
@@ -10,6 +16,24 @@ const taskRouter = require('./tasks');
 const authRouter = require('./auth');
 
 const app = express();
+
+app.get('/', (req, res) => {
+  const application = ReactDOMServer.renderToString(<App />);
+
+  const indexFile = path.resolve('./dist/index.html');
+  fs.readFile(indexFile, 'utf8', (err, data) => {
+    if (err) {
+      console.error('Something went wrong:', err);
+      return res.status(500).send('Oops, better luck next time!');
+    }
+
+    return res.send(
+      data.replace('<div id="root"></div>', `<div id="root">${application}</div>`)
+    );
+  });
+});
+
+app.use(express.static('./build'));
 app.use(bodyParser.json());
 app.use(
   bodyParser.urlencoded({
